1a9e80abd4b79e4b205c42c1df3f5c70e207efab
tran cong phuoc <teosake.1999@gmail.com>
2021-03-18 09:18:52 +0700

    phuoc up

--- process-manager/src/main/java/com/codegym/service/InfoTopicRegisterService.java
+++ process-manager/src/main/java/com/codegym/service/InfoTopicRegisterService.java
@@ -1,4 +1,11 @@
 package com.codegym.service;
 
+import com.codegym.entity.InfoTopicRegister;
+
+import java.util.List;
+
 public interface InfoTopicRegisterService {
+    List<InfoTopicRegister> getListProcess();
+
+    InfoTopicRegister getProcessDetailById(Integer id);
 }
--- process-manager/src/main/java/com/codegym/service/impl/InfoTopicRegisterServiceImpl.java
+++ process-manager/src/main/java/com/codegym/service/impl/InfoTopicRegisterServiceImpl.java
@@ -1,8 +1,26 @@
 package com.codegym.service.impl;
 
+import com.codegym.entity.InfoTopicRegister;
+import com.codegym.repository.InfoTopicRegisterRepository;
 import com.codegym.service.InfoTopicRegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class InfoTopicRegisterServiceImpl implements InfoTopicRegisterService {
+
+    @Autowired
+    private InfoTopicRegisterRepository infoTopicRegisterRepository;
+
+    @Override
+    public List<InfoTopicRegister> getListProcess() {
+        return infoTopicRegisterRepository.findAll();
+    }
+
+    @Override
+    public InfoTopicRegister getProcessDetailById(Integer id) {
+        return infoTopicRegisterRepository.findById(id).orElse(null);
+    }
 }
