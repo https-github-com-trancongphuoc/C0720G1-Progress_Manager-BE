9ea1c67b4f054ae56208f70d7599d9afb69e42e8
nvankhanh97 <nvankhanh97@gmail.com>
2021-03-19 08:54:43 +0700

    khánh 19/3

--- process-manager/src/main/java/com/codegym/repository/StudentRepository.java
+++ process-manager/src/main/java/com/codegym/repository/StudentRepository.java
@@ -1,7 +1,28 @@
 package com.codegym.repository;
 
 import com.codegym.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    Page<Student> findAll(Pageable pageable);
+
+    Page<Student> findAllByGroupAccountIsNull(Pageable pageable);
+
+    @Query(
+            value = "select *\n" +
+                    "from student st \n" +
+                    "where group_account_id is null\n" +
+                    "having concat(st.id,st.`name`) like %?1%",
+            nativeQuery = true)
+    Page<Student> searchStudent(String name, Pageable pageable);
+
+
+
 }
