53b8f902f77bf2a38d473cb3d39c1cb9803a73a5
nvankhanh97 <nvankhanh97@gmail.com>
2021-03-24 17:07:21 +0700

    khanh 24/3

--- process-manager/src/main/java/com/codegym/entity/Student.java
+++ process-manager/src/main/java/com/codegym/entity/Student.java
@@ -19,6 +19,7 @@ public class Student {
     private String email;
     private String avatar;
     private Boolean gender;
+    private Boolean status_join;
 
     @ManyToOne
     @JoinColumn(name = "grade_id", referencedColumnName = "id")
@@ -36,7 +37,7 @@ public class Student {
     public Student() {
     }
 
-    public Student(Integer id, String name, String dateOfBirth, String address, String phone, String email, String avatar, Boolean gender, Grade grade, GroupAccount groupAccount, Account account) {
+    public Student(Integer id, String name, String dateOfBirth, String address, String phone, String email, String avatar, Boolean gender, Boolean status_join, Grade grade, GroupAccount groupAccount, Account account) {
         this.id = id;
         this.name = name;
         this.dateOfBirth = dateOfBirth;
@@ -45,6 +46,7 @@ public class Student {
         this.email = email;
         this.avatar = avatar;
         this.gender = gender;
+        this.status_join = status_join;
         this.grade = grade;
         this.groupAccount = groupAccount;
         this.account = account;
@@ -112,6 +114,14 @@ public class Student {
 
     public void setGender(Boolean gender) {
         this.gender = gender;
+    }
+
+    public Boolean getStatus_join() {
+        return status_join;
+    }
+
+    public void setStatus_join(Boolean status_join) {
+        this.status_join = status_join;
     }
 
     public Grade getGrade() {
--- process-manager/src/main/java/com/codegym/service/GroupAccountService.java
+++ process-manager/src/main/java/com/codegym/service/GroupAccountService.java
@@ -8,18 +8,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-import com.codegym.entity.GroupAccount;
 
 public interface GroupAccountService {
-    void saveGroup(String name, Integer leaderId);
+    void saveGroup(Integer leaderId,String nameGroup);
 
     void acceptJoinGroup(Integer groupId, Integer accountId);
 
     Page<GroupAccount> listGroup(Pageable pageable);
 
-    Page<GroupAccount> searchGroup(String name,Pageable pageable);
+    Page<GroupAccount> searchGroup(String name, Pageable pageable);
 
-    void deleteGroup(Integer groupId);
+    void deleteGroup(Integer groupId,List<Integer> integerList);
 
     List<StudentInformation> getStudentGroup(Integer groupId);
 
@@ -28,4 +27,6 @@ public interface GroupAccountService {
     void acceptGroup(Integer groupId);
 
     GroupAccount getGroupById(Integer id);
+
+    void createGroup(String nameGroup, List<Student> studentList);
 }
